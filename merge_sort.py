5304979bb30408521375eeab2d3917738c85ab20
Max Zinkus <mzinkus@calpoly.edu>
2015-11-10 08:49:29 -0800

    merge sort isn't as cool as quicksort

---
+++ merge_sort.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# O(n log n)
+# merge_sort recurses on n / 2 lists
+# culminating with a merge which traverses n elements
+# thus, we have an O(log N) algorithm which calls an O(n) algorithm
+# O(N log N)
+
+def merge(arr1, arr2): # effectively traverses arr => O(n)
+    ret = []
+    while arr1 and arr2:
+        if arr1[0] <= arr2[0]:
+            ret.append(arr1.pop(0))
+        else:
+            ret.append(arr2.pop(0))
+    
+    while arr1:
+        ret.append(arr1.pop(0))
+
+    while arr2:
+        ret.append(arr2.pop(0))
+    
+    return ret
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    ndx = int(len(arr) / 2)
+    left = []
+    right = []
+
+    for i in range(ndx): # n / 2
+        left.append(arr[i])
+
+    for i in range(ndx, len(arr)): # n / 2
+        right.append(arr[i])
+
+    left = merge_sort(left) # recurse on half
+    right = merge_sort(right) # recurse on half
+
+    return merge(left, right)
+
+if __name__ == '__main__':
+    from random import randrange
+    from sys import argv
+    try:
+        i = int(argv[1])
+    except:
+        i = 32
+    test = [randrange(1000) for x in range(i)]
+    if not "-q" in argv:
+        print("Initial random list:")
+        print(test)
+        print("Sorted list:")
+        print(merge_sort(test))
